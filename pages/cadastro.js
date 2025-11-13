4163e00e9667c74666913312f828de0940fdb06c
Raul Egas <raul.sobrinho@eaportal.org>
2025-11-13 00:17:30 -0300

    Salvament das informações no banco de dados

---
+++ pages/cadastro.js
@@ -0,0 +1,44 @@
+const button = document.querySelector("button")
+button.onclick = (event) => {
+    event.preventDefault()
+    signUpEagle()
+}
+async function signUpEagle() {
+    const name = document.querySelector("#name").value
+    const email = document.querySelector("#email").value
+    const age = document.querySelector("#age").value
+    const nickname = document.querySelector("#nickname").value
+    const password = document.querySelector("#password").value
+
+
+
+    if (name === "" || email === "" || age === "" || nickname === "" || password === "") {
+        alert("Preencha TODAS as informações!")
+        return
+    }
+
+    const user = {
+        name,
+        email,
+        age,
+        nickname,
+        password
+    }
+
+    console.log(user)
+
+    //enviar o user para o backend
+    const response = await fetch("http://localhost:3333/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ user })
+    }).then(response => response.json())
+
+    const { message } = response
+
+    alert(message)
+
+    window.location.href = "../front.html"
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
 import express from "express"
 import cors from "cors"
-import {persons} from "./persons.js"
+import mysql from "mysql2"
+import { persons } from "./persons.js"
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
 
 const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response) => {
     response.json(persons)
 })
 
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, nickname, password } = request.body.user
+    //Cadastrar usuario no backend
+    const insertCommand = `
+    INSERT INTO raulegas_02ta(name, email, age, nickname, password)
+    VALUES (?, ?, ?, ?, ?)
+    `
+
+    database.query(insertCommand, [name, email, age, nickname, password], (error) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.status(201).json({ message: "Usuario cadastrado com sucesso!" })
+    })
+
+
+})
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}!`)
+})
+
+const database = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    connectionLimit: 10
 })
